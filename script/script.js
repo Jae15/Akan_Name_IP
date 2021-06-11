c257b5375a92f96fb9f26c0be89a94d60c765c7f
Janet Mwangi <janomwangi@gmail.com>
2021-06-11 16:03:02 +0300

    Added javascript content

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
+function() {
 
+    let century=document.getElementById(cc).nodeValue
+    let year=document.getElementById(yy)
+    let month=document.getElementById(mm)
+    let day=document.getElementById(dd)
+
+    if(month <1 && month > 12){
+        document.getElementById("output").innerHTML="input a correct month"
+   } else if(dd< 0 && dd >31){document.getElementById("output").innerHTML="input a valid day of the month"
+}
+
+
+
+}
