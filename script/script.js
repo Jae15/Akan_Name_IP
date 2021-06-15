6a039d680bdd371576d18318121bb36c00756f42
Janet Mwangi <janomwangi@gmail.com>
2021-06-15 13:39:35 +0300

    changes on js and html

--- script/script.js
+++ script/script.js
@@ -7,37 +7,47 @@ function calculate(){
   var gender= document.getElementById("Gender").value;
   var maleAkanNames=[" Kwasi", " Kwadwo", " Kwabena", " Kwaku", " Yaw", " Kofi", " Kwame"]
   var femaleAkanNames=[" Akosua", " Adwoa", " Abenaa", " Akua", " Yaa", " Afua", " Ama"]
+
+
+
+  let yourDayOfWeek;
   
   if(cc >= 19 && cc<= 20){
   
-  let yourDayOfWeek = Math.round((((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )%7)
-  
+   yourDayOfWeek = Math.round((((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )%7)
+
+   if(gender == "male")
+   console.log('gender :>> ', gender);
+   console.log("your Akan name is" +maleAkanNames[0])
+}
   console.log(yourDayOfWeek)
   }
-  if(yourDayOfWeek === 1){
-    if(gender == male)
+  if(yourDayOfWeek === 1 && gender==="male"){
+  
           console.log("your Akan name is" +maleAkanNames[0])
     }
-    else if( gender == female){
-    console.log("your Akan name is" + femaleAkanaNames[0])
+    if(yourDayOfWeek === 1 && gender==="female"){
+    console.log("your Akan name is" +femaleAkanaNames[0])
     }
     else{
       console.log("No Akan Name for you")
     }
-  }
+
+    if(yourDayOfWeek === 2){
+      if(gender == "male"){
+            console.log("your Akan name is" +maleAkanaNames[1])
+      }
+      else if( gender == "female"){
+      console.log("your Akan name is" +femaleAkanaNames[1])
+      }
+      else{
+        console.log("your Akan name is")
+      }
+    }
+  
 
 
-//    if(yourDayOfWeek === 2){
-//     if(gender == male){
-//           console.log("your Akan name is" +maleAkanaNames[1])
-//     }
-//     else if( gender == female){
-//     console.log("your Akan name is" +femaleAkanaNames[1])
-//     }
-//     else{
-//       console.log("your Akan name is")
-//     }
-//   }
+  
 //  else if(yourDayOfWeek === 3){
     
 //     if(gender == male){
