ca33975d64c35303eebc94f6077fefc6f78f9de8
Janet Mwangi <janomwangi@gmail.com>
2021-06-15 14:39:37 +0300

    Added changes on javascript

--- script/script.js
+++ script/script.js
@@ -1,125 +1,74 @@
 function calculate(){
 
-  var dd= document.getElementById("dd").value;
-  var mm= document.getElementById("mm").value;
-  var cc= document.getElementById("cc").value;
-  var yy= document.getElementById("yy").value;
+  var dd= Number(document.getElementById("dd").value);
+  var mm= Number(document.getElementById("mm").value);
+  var cc= Number(document.getElementById("cc").value);
+  var yy= Number(document.getElementById("yy").value);
   var gender= document.getElementById("Gender").value;
   var maleAkanNames=[" Kwasi", " Kwadwo", " Kwabena", " Kwaku", " Yaw", " Kofi", " Kwame"]
   var femaleAkanNames=[" Akosua", " Adwoa", " Abenaa", " Akua", " Yaa", " Afua", " Ama"]
 
 
+// make absolute to remove negative calculations
+  var yourDayOfWeek =  Math.abs(Math.round((((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )%7));
 
-  let yourDayOfWeek;
+  console.log(`yourDayOfWeek`, yourDayOfWeek)
   
-  if(cc >= 19 && cc<= 20){
-  
-   yourDayOfWeek = Math.round((((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd )%7)
+  if(cc === 19 || cc === 20){
+    console.log(`cc`, cc)
+    if(yourDayOfWeek === 0) {
+      if(gender === 'male') {
+        console.log("your Akan name is" +maleAkanNames[yourDayOfWeek])
+      }
+      if(gender === 'female'){
+        console.log("your Akan name is" +femaleAkanNames[yourDayOfWeek])
+      }
+    }
 
-   if(gender == "male")
-   console.log('gender :>> ', gender);
-   console.log("your Akan name is" +maleAkanNames[0])
-}
-  console.log(yourDayOfWeek)
+    if(yourDayOfWeek === 1) {
+      if(gender === 'male') {
+        console.log("your Akan name is" +maleAkanNames[yourDayOfWeek])
+      }
+      if(gender === 'female'){
+        console.log("your Akan name is" +femaleAkanNames[yourDayOfWeek])
+      }
+    }
+
+    if(yourDayOfWeek === 2) {
+      if(gender === 'male') {
+        console.log("your Akan name is" +maleAkanNames[yourDayOfWeek])
+      }
+      if(gender === 'female'){
+        console.log("your Akan name is" +femaleAkanNames[yourDayOfWeek])
+      }
+    }
+
+    if(yourDayOfWeek === 3) {
+      if(gender === 'male') {
+        console.log("your Akan name is" +maleAkanNames[yourDayOfWeek])
+      }
+      if(gender === 'female'){
+        console.log("your Akan name is" +femaleAkanNames[yourDayOfWeek])
+      }
+    }
+
+    if(yourDayOfWeek === 4) {
+      if(gender === 'male') {
+        console.log("your Akan name is" +maleAkanNames[yourDayOfWeek])
+      }
+      if(gender === 'female'){
+        console.log("your Akan name is" +femaleAkanNames[yourDayOfWeek])
+      }
+    }
+    if(yourDayOfWeek === 5) {
+      if(gender === 'male') {
+        console.log("your Akan name is" +maleAkanNames[yourDayOfWeek])
+      }
+      if(gender === 'female'){
+        console.log("your Akan name is" +femaleAkanNames[yourDayOfWeek])
+      }
+    }
+
   }
-  if(yourDayOfWeek === 1 && gender==="male"){
-  
-          console.log("your Akan name is" +maleAkanNames[0])
-    }
-    if(yourDayOfWeek === 1 && gender==="female"){
-    console.log("your Akan name is" +femaleAkanaNames[0])
-    }
-    else{
-      console.log("No Akan Name for you")
-    }
 
-    if(yourDayOfWeek === 2){
-      if(gender == "male"){
-            console.log("your Akan name is" +maleAkanaNames[1])
-      }
-      else if( gender == "female"){
-      console.log("your Akan name is" +femaleAkanaNames[1])
-      }
-      else{
-        console.log("your Akan name is")
-      }
-    }
-  
-
-
-  
-//  else if(yourDayOfWeek === 3){
-    
-//     if(gender == male){
-//           console.log("your Akan name is" +maleAkanaNames[2])
-//     }
-//     else if( gender == female){
-//     console.log("Your Akan name is" +femaleAkanaNames[2])
-//     }
-//     else{
-//       console.log("No Akan Name for you")
-//     }
-//   }
-//   else if(yourDayOfWeek  ===4){
-    
-//     if(gender == male){
-//           console.log("Your Akan name is" +maleAkanaNames[3])
-//     }
-//     else if( gender == female){
-//     console.log("Your Akan name is" +femaleAkanaNames[3])
-//     }
-//     else{
-//       console.log("No Akan Name for you")
-//     }
-//   }
-//   else if(yourDayOfWeek === 5){
-    
-//     if(gender == male){
-//           console.log("Your Akan name is" +maleAkanaNames[4])
-//     }
-//     else if( gender == female){
-//     console.log("Your Akan name is" +femaleAkanaNames[4])
-//     }
-//     else{
-//       console.log("No Akan Name for you")
-//     }
-//   }
-//   else if(yourDayOfWeek  === 6){
-   
-//     if(gender == male){
-//           console.log("Your Akan name is" +maleAkanaNames[5])
-//     }
-//    else if( gender == female){
-//     console.log("Your Akan name is" +femaleAkanaNames[5])
-//     }
-//     else{
-//       console.log("No Akan Name for you")
-//     }
-//   }
-//  else if(yourDayOfWeek === 7){
-  
-//     if(gender == male){
-//           console.log("Your Akan name is" +maleAkanaNames[6])
-//     }
-//     else if( gender == female){
-//     console.log("Your Akan name is" +femaleAkanaNames[6])
-//     }
-//     else{
-//       console.log("No Akan Name for you")
-//     }
-//   }
-// else{
-//   console.log("wrong calculation")
-// }
-//     // console.log("you proceed")
-//   }
-//   else{
-//     console.log("input a valid century")
-//   }
-// ;
-// }
-// calculate()
-//   }
-
-  
-  
+}
